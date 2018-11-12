b308886adbbbe515093b89dab65d4dde72af79e5
soisausem <38421092+soisausem@users.noreply.github.com>
2018-11-12 17:32:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,15 @@
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.promit('数字当てゲーム。0～3の数字を入れよう！'));
 
+var message;
+if(answer === number){
+	message = 'あたり！';
+}else if(answer < number){
+	message = '残念！もっと大きいです';
+}else if(number < answer){
+	message = '残念！もっと小さいです';
+}else{
+	message = '0～3の数字を入れよう';
+}
+
+document.getElementById('choice').textContent = message;
